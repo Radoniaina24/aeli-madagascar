5b6355ab0d681bf7231a3308943df3ccd110dae5
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-05-08 08:11:24 +0300

    v3.3

--- src/features/candidate/index.tsx
+++ src/features/candidate/index.tsx
@@ -6,7 +6,7 @@ import Pagination from "./Pagination";
 
 export default function Candidate() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8 z-10">
+    <div className=" bg-gray-50 py-8 z-10">
       <div className="px-4 sm:px-6 lg:px-6">
         <div className="bg-white rounded-lg shadow-md overflow-hidden">
           {/* En-tête et filtres */}
